e3243454630895c20ae574c5e38e649dc7499ac5
Андрей Ильин <a.ilin.v@yandex.ru>
2023-03-24 17:22:18 +0300

    upd::func

--- app/functions.js
+++ app/functions.js
@@ -1,39 +1,59 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.functionsAnswers = {
-  argsAsArray: function(fn, arr) {
+    argsAsArray: function (fn, arr) {
+        return fn(...arr);
+    },
 
-  },
+    speak: function (fn, obj) {
+        fn = fn.bind(obj);
+        return fn();
+    },
 
-  speak: function(fn, obj) {
+    functionFunction: function (str) {
+        return function (str2) {
+            return str + ", " + str2;
+        };
+    },
 
-  },
+    makeClosures: function (arr, fn) {
+        return arr.map(
+            (val) =>
+                function () {
+                    return fn(val);
+                }
+        );
+    },
 
-  functionFunction: function(str) {
+    partial: function (fn, str1, str2) {
+        return fn.bind(null, str1, str2);
+    },
 
-  },
+    useArguments: function () {
+        return Array.from(arguments).reduce((sum, val) => sum + val, 0);
+    },
 
-  makeClosures: function(arr, fn) {
+    callIt: function (fn, ...args) {
+        return fn(...args);
+    },
 
-  },
+    partialUsingArguments: function (fn, ...args) {
+        return function (...args2) {
+            return fn(...args, ...args2);
+        };
+    },
 
-  partial: function(fn, str1, str2) {
-
-  },
-
-  useArguments: function() {
-
-  },
-
-  callIt: function(fn) {
-
-  },
-
-  partialUsingArguments: function(fn) {
-
-  },
-
-  curryIt: function(fn) {
-
-  }
+    curryIt: function (fn) {
+        let arg = [];
+        function f(a) {
+            arg = arg.concat(a);
+            if (arg.length == fn.length) {
+                let result = fn(...arg);
+                arg = [];
+                return result;
+            }
+            return f;
+        }
+        return f;
+    },
 };

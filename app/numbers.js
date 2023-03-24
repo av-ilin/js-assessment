842cfded4c071493789b97d58f4c5302b4664b62
Андрей Ильин <a.ilin.v@yandex.ru>
2023-03-24 18:35:13 +0300

    upd::nums

--- app/numbers.js
+++ app/numbers.js
@@ -1,19 +1,26 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.numbersAnswers = {
-  valueAtBit: function(num, bit) {
+    valueAtBit: function (num, bit) {
+        let bitPosition = bit - 1;
+        return (num >> bitPosition) & 1;
+    },
 
-  },
+    base10: function (str) {
+        return parseInt(str, 2);
+    },
 
-  base10: function(str) {
+    convertToBinary: function (num) {
+        let bits = [];
+        for (let i = 7; i >= 0; i--) bits.push((num >> i) & 1);
+        return bits.join("");
+    },
 
-  },
-
-  convertToBinary: function(num) {
-
-  },
-
-  multiply: function(a, b) {
-
-  }
+    multiply: function (a, b) {
+        let c1 = Math.pow(10, Math.floor(Math.log(a) * -1));
+        let c2 = Math.pow(10, Math.floor(Math.log(b) * -1));
+        a *= c1;
+        b *= c2;
+        return (a * b) / (c1 * c2);
+    },
 };

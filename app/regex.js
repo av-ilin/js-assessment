b46376691faf9b412ddcf60edaf6118daa002cbc
Андрей Ильин <a.ilin.v@yandex.ru>
2023-03-24 23:17:04 +0300

    upd::regex

--- app/regex.js
+++ app/regex.js
@@ -1,27 +1,28 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.regexAnswers = {
-  containsNumber: function(str) {
+    containsNumber: function (str) {
+        return /\d/.test(str);
+    },
 
-  },
+    containsRepeatingLetter: function (str) {
+        return /([a-zA-z])\1/.test(str);
+    },
 
-  containsRepeatingLetter: function(str) {
+    endsWithVowel: function (str) {
+        return /[aeiou]$/i.test(str);
+    },
 
-  },
+    captureThreeNumbers: function (str) {
+        let match = str.match(/\d{3}/);
+        return match ? match[0] : false;
+    },
 
-  endsWithVowel: function(str) {
+    matchesPattern: function (str) {
+        return /^\d{3}-\d{3}-\d{4}$/.test(str);
+    },
 
-  },
-
-  captureThreeNumbers: function(str) {
-
-  },
-
-  matchesPattern: function(str) {
-
-  },
-
-  isUSD: function(str) {
-
-  }
+    isUSD: function (str) {
+        return /^\$\d{1,3}(,\d{3})*(\.\d{2})?$/.test(str);
+    },
 };

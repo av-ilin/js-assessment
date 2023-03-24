f8f92264ea808f0152d4e796ce8d41c287142a5a
Андрей Ильин <a.ilin.v@yandex.ru>
2023-03-24 15:08:38 +0300

    upd::count

--- app/count.js
+++ app/count.js
@@ -1,7 +1,25 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.countAnswers = {
-  count: function (start, end) {
-
-  }
+    count: function (start, end) {
+        let counter = {
+            id: undefined,
+            start: start,
+            end: end,
+            go() {
+                console.log(this.start);
+                let cur = start + 1;
+                this.id = setInterval(() => {
+                    console.log(cur);
+                    cur++;
+                    if (cur > this.end) clearInterval(this.id);
+                }, 100);
+            },
+            cancel() {
+                clearInterval(this.id);
+            },
+        };
+        counter.go();
+        return counter;
+    },
 };

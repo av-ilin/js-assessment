06c6476f49c857bf7343371abaa7c15f52617139
Андрей Ильин <a.ilin.v@yandex.ru>
2023-03-24 15:21:28 +0300

    upd::flow

--- app/flowControl.js
+++ app/flowControl.js
@@ -1,15 +1,24 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.flowControlAnswers = {
-  fizzBuzz: function(num) {
-    // write a function that receives a number as its argument;
-    // if the number is divisible by 3, the function should return 'fizz';
-    // if the number is divisible by 5, the function should return 'buzz';
-    // if the number is divisible by 3 and 5, the function should return
-    // 'fizzbuzz';
-    //
-    // otherwise the function should return the number, or false if no number
-    // was provided or the value provided is not a number
-
-  }
+    fizzBuzz: function (num) {
+        switch (true) {
+            case num % 3 == 0 && num % 5 == 0:
+                return "fizzbuzz";
+            case num % 3 == 0:
+                return "fizz";
+            case num % 5 == 0:
+                return "buzz";
+            default:
+                return typeof num == "number" ? num : false;
+        }
+        // write a function that receives a number as its argument;
+        // if the number is divisible by 3, the function should return 'fizz';
+        // if the number is divisible by 5, the function should return 'buzz';
+        // if the number is divisible by 3 and 5, the function should return
+        // 'fizzbuzz';
+        //
+        // otherwise the function should return the number, or false if no number
+        // was provided or the value provided is not a number
+    },
 };
